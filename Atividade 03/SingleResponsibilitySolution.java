2952980c01e14ea0d8890abff428a95451f0e7e4
Matheus dos Santos Oliveira <shen.harzgard@gmail.com>
2025-02-18 20:54:23 -0300

    More Solutions
    - OpenClosedSolution
    - SingleResponsabilitySolution
    - SolutionOfEncapsulation
    - TightCouplicgSolution

---
+++ Atividade 03/SingleResponsibilitySolution.java
@@ -0,0 +1,47 @@
+import javax.xml.crypto.Data;
+
+public class SingleResponsibilitySolution
+{
+    public static void main(String[] args)
+    {
+        Invoice invoice = new Invoice(1000);
+        InvoicePrinter invoicePrinter = new InvoicePrinter();
+        InvoiceDatabase invoiceDatabase = new InvoiceDatabase();
+
+        invoicePrinter.printInvoice(invoice);
+        invoiceDatabase.saveToDatabase(invoice);
+    }
+
+}
+
+class Invoice
+{
+    private double amount;
+
+    public Invoice(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+}
+
+class InvoicePrinter
+{
+    public void printInvoice(Invoice invoice)
+    {
+        System.out.println("Invoice amount: " + invoice.getAmount());
+    }
+}
+
+class InvoiceDatabase
+{
+    public void saveToDatabase(Invoice invoice)
+    {
+        System.out.println("Saving invoice to database...");
+    }
+}
+
