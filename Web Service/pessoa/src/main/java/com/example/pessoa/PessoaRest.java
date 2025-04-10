48ddcd698d149882cf4b3a15c96870a5735ece37
Matheus dos Santos Oliveira <shen.harzgard@gmail.com>
2025-04-10 12:27:05 -0300

    Imports de Delete

--- Web Service/pessoa/src/main/java/com/example/pessoa/PessoaRest.java
+++ Web Service/pessoa/src/main/java/com/example/pessoa/PessoaRest.java
@@ -3,7 +3,10 @@ package com.example.pessoa;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
